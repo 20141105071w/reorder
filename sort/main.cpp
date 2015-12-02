ff1165b3e4eb6c5f03b7c37a1007dcc72a319d43
20141105071 <20141105071wyr@mac26.local>
2015-12-02 18:23:56 +0800

    Initial Commit

---
+++ sort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sort
+//
+//  Created by 20141105071 on 15/12/2.
+//  Copyright © 2015年 20141105071. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
